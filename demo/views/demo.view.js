827586650e35e7cc04a138e09fdcb2ca155062d2
fireyy <fireyy@gmail.com>
2015-05-07 11:22:29 +0800

    测试view

---
+++ demo/views/demo.view.js
@@ -0,0 +1,21 @@
+(function () {
+
+  this.Demo = this.Demo || {};
+
+  this.Demo.View = _.inherit({
+    initialize: function (options) {
+      this.templateFn = _.template(options.template);
+      this.$el = $(options.container);
+
+      _.extend(this, options);
+    },
+
+    render: function (data) {
+      var html = this.templateFn(data);
+      this.$el.html(html);
+      this.bindEvents();
+    }
+
+  });
+
+}).call(this);
---
+++ demo/views/demo.view.sidebar.js
@@ -0,0 +1,20 @@
+(function(){
+
+  this.Demo = this.Demo || {};
+
+  this.Demo.SidebarView = _.inherit(this.Demo.View, {
+    initialize: function ($super, options) {
+      options.template = [
+        '<ul class="sidebar">',
+          '<% _.each(items, function(item){%>',
+          '<li><a href="<%=item.url%>"><%=item.title%></a></li>',
+          '<%})%>',
+        '</ul>'
+      ];
+      options.container = '#js-sidebar-container';
+
+      $super(options);
+    }
+  });
+
+}).call(this);
